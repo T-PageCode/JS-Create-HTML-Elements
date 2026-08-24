edb6b8688680b3e4dbf4b1a59ac91513c0741bda
T-PageCode Developer <cdt20260@outlook.com>
2026-08-24 18:12:58 +0800

    增加小字“T-PageCode”

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ document.addEventListener("keydown", (e) => {
         e.preventDefault();
     }
 });
+document.body.style.backgroundColor = "white";
 const titleText = document.createElement("h1");
+const T_PageCode = document.createElement("p");
 document.body.style.userSelect = "none";
 titleText.id = "title-text";
 titleText.style.position = "absolute";
@@ -19,3 +21,12 @@ titleText.style.transform = "translate(-50%, -50%)";
 titleText.textContent = "尝试使用JavaScript创建HTML元素";
 titleText.style.fontWeight = "200";
 document.body.appendChild(titleText);
+T_PageCode.id = "title-text";
+T_PageCode.style.position = "absolute";
+T_PageCode.style.top = "160px";
+T_PageCode.style.left = "50%";
+T_PageCode.style.transform = "translate(-50%, -50%)";
+T_PageCode.textContent = "T-PageCode";
+T_PageCode.style.fontWeight = "200";
+T_PageCode.style.fontSize = "20px";
+document.body.appendChild(T_PageCode);
