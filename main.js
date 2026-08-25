c5e300e7f416fac6dc29eabc33fed405f00389d1
T-PageCode Developer <cdt20260@outlook.com>
2026-08-25 08:50:10 +0800

    增肌蓝色色块，把新的按钮放进这里了

--- main.js
+++ main.js
@@ -13,29 +13,29 @@ function addButtonElement() {
     let newBtn = document.createElement("button");
     newBtn.textContent = "这是一个新的按钮";
     newBtn.style.display = "block";
-    newBtn.style.margin = "10px";
+    newBtn.style.margin = "10px auto";
     newBtn.style.width = "150px";
     newBtn.style.height = "30px";
     newBtn.style.borderRadius = "10px";
     newBtn.style.border = "none";
     newBtn.style.backgroundColor = "rgb(0,50,255)";
     newBtn.style.color = "white";
-    document.body.appendChild(newBtn);
+    div.appendChild(newBtn);
 }
-document.body.style.backgroundColor = "white";
+document.body.style.background = "linear-gradient(180deg,rgb(0, 247, 235),rgb(140, 0, 255))";
+document.body.style.backgroundAttachment = "fixed";
 const titleText = document.createElement("h1");
 const T_PageCode = document.createElement("p");
 const addBtn = document.createElement("button");
 document.body.style.userSelect = "none";
-titleText.id = "title-text";
 titleText.style.position = "absolute";
 titleText.style.top = "100px";
 titleText.style.left = "50%";
 titleText.style.transform = "translate(-50%, -50%)";
 titleText.textContent = "尝试使用JavaScript创建HTML元素";
 titleText.style.fontWeight = "200";
+titleText.style.color = "white";
 document.body.appendChild(titleText);
-T_PageCode.id = "title-text";
 T_PageCode.style.position = "absolute";
 T_PageCode.style.top = "160px";
 T_PageCode.style.left = "50%";
@@ -43,8 +43,9 @@ T_PageCode.style.transform = "translate(-50%, -50%)";
 T_PageCode.textContent = "T-PageCode";
 T_PageCode.style.fontWeight = "200";
 T_PageCode.style.fontSize = "20px";
+T_PageCode.style.color = "white";
 document.body.appendChild(T_PageCode);
-addBtn.textContent = "在此页面上增加一个按钮"
+addBtn.textContent = "在浅蓝色的色块上增加按钮"
 addBtn.style.position = "absolute";
 addBtn.style.top = "250px";
 addBtn.style.left = "50%";
@@ -74,3 +75,32 @@ addBtn.onclick = () => {
     addButtonElement();
 }
 document.body.appendChild(addBtn);
+let div = document.createElement("div");
+div.style.position = "absolute";
+div.style.top = "450px";
+div.style.left = "50%";
+div.style.transform = "translate(-50%,-50%)";
+div.style.width = "300px";
+div.style.height = "250px";
+div.style.backgroundColor = "rgb(121, 235, 255)";
+div.style.borderRadius = "25px";
+div.style.overflow = "auto";
+document.body.appendChild(div);
+let styleTag = document.createElement("style");
+styleTag.textContent = `
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: rgb(222, 204, 255);
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: rgb(206, 154, 255);
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: rgb(218, 162, 255);
+    }
+`;
+document.head.appendChild(styleTag);
